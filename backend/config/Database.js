30713602b4bdcf5dd5698618e212ccb7e09e9927
bayunugroho03 <bayunugroho@uhamka.ac.id>
2026-02-21 23:15:04 +0700

    Configure proper Database settings for Vercel and Cloud MySQL

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -2,10 +2,19 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+import mysql2 from 'mysql2';
+
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule: mysql2, // Required for Vercel Serverless Functions
+    dialectOptions: {
+        connectTimeout: 60000, // Important for cloud architectures
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 });
 
 export default db;
