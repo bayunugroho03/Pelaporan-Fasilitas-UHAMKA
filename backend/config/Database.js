4832f3267e8cb7c50544188348580688961ae863
bayunugroho03 <bayunugroho@uhamka.ac.id>
2026-02-21 23:49:10 +0700

    Fix TiDB SSL config require: true

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -8,10 +8,10 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
-    dialectModule: mysql2, // Required for Vercel Serverless Functions
+    dialectModule: mysql2,
     dialectOptions: {
-        connectTimeout: 60000, // Important for cloud architectures
         ssl: {
+            require: true,
             rejectUnauthorized: false
         }
     }
